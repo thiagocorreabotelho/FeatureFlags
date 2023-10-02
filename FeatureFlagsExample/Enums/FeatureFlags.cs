a01a9b57de86bc666f41a2b8272fdf88e326366f
Thiago Correa Dev <thiagogcorreabotelho2021@outlook.com>
2023-10-02 20:58:16 -0300

    Commmit inicial

---
+++ FeatureFlagsExample/Enums/FeatureFlags.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace FeatureFlagsExample.Enums
+{
+    public enum FeatureFlags
+    {
+        [Description("FeatureFlagWeatherForecast")]
+        FeatureFlagWeatherForecast = 1,
+    }
+}
---
+++ FeatureFlagsExample/Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace FeatureFlagsExample.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var enumType = value.GetType();
+
+            var fieldInfo = enumType.GetField(value.ToString());
+
+            var descriptionAttribute = (DescriptionAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(DescriptionAttribute));
+
+            if(descriptionAttribute != null) 
+                return descriptionAttribute.Description;
+
+            return value.ToString();
+        }
+    }
+}
